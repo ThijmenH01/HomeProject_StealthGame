af133ef0bb04e478ded9d2c45f628b52d5d7a6c6
Thijmen <Thijmenh01@gmail.com>
2019-09-03 20:59:35 +0100

    Finished with Alpha
    Finished with Alpha

---
+++ StealthGame_Unity/Assets/Recources/Scripts/MapGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Transform[] tilePrefabs;
+    public Transform ground;
+    public Vector2 mapSize;
+    public float tileSize = 10;
+
+    int tileToSpawn;
+
+    void Start(){
+        tileToSpawn = tilePrefabs.Length;
+        GenerateMap();
+    }
+
+    void Update(){
+    }
+
+    public void GenerateMap() {
+        for (int x = 0; x < mapSize.x; x++) {
+            for (int y = 0; y < mapSize.y; y++) {
+                tileToSpawn = Random.Range(0, tilePrefabs.Length);
+                Vector3 _tilePos = new Vector3(x * tileSize + ground.position.x, 0, y * tileSize + ground.position.z);
+                Transform _newTile = Instantiate(tilePrefabs[tileToSpawn], _tilePos, Quaternion.Euler(0,  Random.Range(0, 4)* 45, 0)); 
+                _newTile.transform.parent = transform;
+            }
+        }
+    }
+}
