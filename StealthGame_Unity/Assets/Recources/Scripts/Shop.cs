50f3a825b4db9b7e67e3f88217b98742677b6909
Thijmen <Thijmenh01@gmail.com>
2019-09-08 15:06:39 +0100

    shop changes
    shop changes

---
+++ StealthGame_Unity/Assets/Recources/Scripts/Shop.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+
+    [Header("List of Items selling")]
+    public ShopItem[] shopItem;
+
+    [Header("References")]
+    public Transform shopContainer;
+    public GameObject shopItemPrefab;
+    public Text balancePerm;
+
+    void Start(){
+        FillShop();
+        Player.balancePerm = PlayerPrefs.GetInt("balancePerm");
+        balancePerm.text = Player.balancePerm.ToString();
+    }
+    
+    void Update(){
+    
+        
+    }
+
+    private void FillShop() {
+        for (int i = 0; i < shopItem.Length; i++) {
+            ShopItem si = shopItem[i];
+            GameObject itemObject = Instantiate(shopItemPrefab, shopContainer);
+
+            itemObject.GetComponent<Button>().onClick.AddListener(() => OnButtonClick(si));
+
+            itemObject.GetComponent<Image>().color = si.itemColor;
+            itemObject.transform.GetChild(1).GetComponent<Text>().text = si.itemPrice.ToString();
+
+            //itemObject.transform.GetChild(0).GetComponent<Image>().color = new Color(255, 255, 255, 0);
+        }
+    }
+
+    private void OnButtonClick(ShopItem item) {
+        Debug.Log(item.itemID);
+    }
+}
---
+++ StealthGame_Unity/Assets/Recources/Scripts/ShopItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Shop/Shop Item")]
+public class ShopItem : ScriptableObject
+{
+    public int itemPrice;
+    public bool itemIsOwned;
+    public Color itemColor;
+    public int itemID;
+}
